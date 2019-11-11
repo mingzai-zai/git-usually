c043f2e6a81691dfb89fb87412d9653bff0c24b4
mingzai-zai <2534855401@qq.com>
2019-11-11 09:33:09 +0800

    随机rgb颜色

--- suiji.js
+++ suiji.js
@@ -2,3 +2,10 @@ function randomInt(n, m) {
   var num = Math.floor(Math.random() * (m - n + 1) + n);
   return num;
 }
+
+function randomRgb(n, m) {
+  var r = randomInt(0, 255);
+  var g = randomInt(0, 255);
+  var b = randomInt(0, 255);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
