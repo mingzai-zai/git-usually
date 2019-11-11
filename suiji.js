173e64641a4388ccc6456caf38d707928880c805
mingzai-zai <2534855401@qq.com>
2019-11-11 10:36:05 +0800

    随机十六进制颜色

--- suiji.js
+++ suiji.js
@@ -3,9 +3,40 @@ function randomInt(n, m) {
   return num;
 }
 
-function randomRgb(n, m) {
+function randomRgb() {
   var r = randomInt(0, 255);
   var g = randomInt(0, 255);
   var b = randomInt(0, 255);
   return "rgb(" + r + ", " + g + ", " + b + ")";
 }
+
+function randomColor() {
+  var arr = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+  ];
+
+  let color = ["#"];
+  for (let i = 0; i < 6; i++) {
+    let r = Math.floor(Math.random() * arr.length);
+    r = arr[r];
+    color.push(r);
+  }
+  return color.join("");
+}
+
+console.log(randomColor());
